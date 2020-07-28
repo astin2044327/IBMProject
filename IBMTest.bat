3aedfc80f0654694c474dc6b4786addb25e0cd80
astin2044327 <astin2044327@gmail.com>
2020-07-27 21:18:52 -0400

    Update IBMTest.bat
    change to master

--- IBMTest.bat
+++ IBMTest.bat
@@ -1,10 +1,5 @@
-echo  off
 
-echo "*********************************"
-
-echo "                                  "
-echo "Deleting the folder..."
-rmdir /Q /S c:\server1
-
-echo "Creating the folder..."
-md c:\server1 
+echo  off  
+echo "performing unit test"
+echo "Colleting the infomation..."
+systeminfo >> c:\server1\srvlog.txt
